08f6adb47a8e28598fceb55652934fde06fd51d6
patricky95 <patrickycandy@gmail.com>
2015-10-09 21:08:19 -0400

    Created Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Main extends JFrame {
+	public Main(String title) {
+		
+	}
+	
+	public void init() {
+		
+	}
+
+	public static void main(String[] args) {
+		Main main = new Main("Genetic Reshuffle");
+	}
+
+}

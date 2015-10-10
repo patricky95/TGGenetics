dda9164a1660b31e8c9f6966dac9284718567cde
patricky95 <patrickycandy@gmail.com>
2015-10-09 22:02:02 -0400

    Night 1 Window

--- Main.java
+++ Main.java
@@ -1,10 +1,23 @@
 package main;
 
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
+import javax.swing.JTextField;
 
 public class Main extends JFrame {
+	private static final long serialVersionUID = 1L;
+	
+	public static Dimension size = new Dimension(640, 480);
+	public static JTextField textbox;
+	
 	public Main(String title) {
-		
+		super(title);
+		setSize(size);
+		setVisible(true);
+		setResizable(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
 	}
 	
 	public void init() {
